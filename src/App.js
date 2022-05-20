949d920774cb6e83853eae5a0f4c1310281bc6d0
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-05-20 23:54:24 +0530

    useQuery hook, react query states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+
+import { useQuery } from "react-query";
+import "./App.css";
+
+function Button() {
+  const { data, error } = useQuery("my-key", () => {
+    // n/w request function
+    return new Promise(resolve => {
+      setTimeout(() => resolve(Math.random), 3000);
+    });
+  });
+  console.log({ data, error });
+  return <button>I am a button {data}</button>;
+}
 
 function App() {
+  const [visible, setVisible] = useState(true);
+
+  function toggleButton() {
+    setVisible(visible => !visible);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        {visible && <Button />}
+        <button onClick={toggleButton}>Toggle</button>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
